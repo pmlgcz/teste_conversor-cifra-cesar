e9ad1ec0926a3bd0b24b7b24d9eeee21ada4a6ee
Pamela Cruz <117772631+pmlgcz@users.noreply.github.com>
2022-11-29 23:11:55 -0300

    Add files via upload

---
+++ crip.js
@@ -0,0 +1,31 @@
+let encryptBtn = document.getElementById('encrypt-btn');
+let eInput = document.getElementById('encryptedInput-1');
+let pInput = document.getElementById('plainInput');
+let inputs = [eInput, pInput]
+let copyBtn = document.getElementById('copyToClipBoard');
+
+inputs.forEach( input => {
+				input.oninput = () => {
+								input.value = input.value.toUpperCase()
+				}
+})
+
+function encrypt() {
+				let pInput = document.getElementById('plainInput-1').value;
+				let solved =''
+				let shiftInput = parseInt(document.getElementById('shiftInput').value)
+				for (var i = 0; i< pInput.length; i++){
+								let ascii_num = pInput[i].charCodeAt()
+								let sum = ascii_num + shiftInput
+								sum>= 65 && <=90 ? solved += String.fromCharCode(sum) : sum > 90 ? solved += String.fromCharCode(65 + (sum & 91)) : solved += pInput [i]
+				}
+				eInput.value = solved
+}
+function copyText() {
+				eInput.select()
+				eInput.setSelectionRange(0,99999)
+				document.execComand('copy')
+				alert('Copiado para o clipboard: ${eInput.value}')
+}
+				copyBtn.addEventListener('click', copyText)
+				encryptBtn.addEventListener('click', encrypt)
